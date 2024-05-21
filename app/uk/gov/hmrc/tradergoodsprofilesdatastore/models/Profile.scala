696bd76424311baae6e714d44c08b97d6d173a63
Ewan-James-Donovan <34974028+Ewan-James-Donovan@users.noreply.github.com>
2024-05-21 09:53:04 +0100

    TGP-1038: Add two files for start

---
+++ app/uk/gov/hmrc/tradergoodsprofilesdatastore/models/Profile.scala
@@ -0,0 +1,1 @@
+package uk.gov.hmrc.tradergoodsprofilesdatastore.models case class Profile ()
---
+++ app/uk/gov/hmrc/tradergoodsprofilesdatastore/repositories/ProfileRepository.scala
@@ -0,0 +1,5 @@
+package uk.gov.hmrc.tradergoodsprofilesdatastore.repositories
+
+class ProfileRepository {
+
+}
