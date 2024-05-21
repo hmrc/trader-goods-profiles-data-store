8ce907762f791e70b66c22c77195cf721767fba9
Ewan-James-Donovan <34974028+Ewan-James-Donovan@users.noreply.github.com>
2024-05-21 10:00:52 +0100

    TGP-1038: Profile model, first pass

--- app/uk/gov/hmrc/tradergoodsprofilesdatastore/models/Profile.scala
+++ app/uk/gov/hmrc/tradergoodsprofilesdatastore/models/Profile.scala
@@ -1,1 +1,13 @@
-package uk.gov.hmrc.tradergoodsprofilesdatastore.models case class Profile ()
+package uk.gov.hmrc.tradergoodsprofilesdatastore.models
+
+import play.api.libs.json.{Json, OFormat}
+
+case class Profile (
+                     ukims: String,
+                     nirms: Option[String],
+                     niphl: Option[String]
+                   )
+
+object Profile {
+  implicit val format: OFormat[Profile] = Json.format[Profile]
+}
