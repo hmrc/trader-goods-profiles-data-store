94186fe8f90af416eb8d1af193764f1beb3347b7
Ewan-James-Donovan <34974028+Ewan-James-Donovan@users.noreply.github.com>
2024-05-21 11:22:23 +0100

    TGP-1038: Example usage

--- app/uk/gov/hmrc/tradergoodsprofilesdatastore/controllers/MicroserviceHelloWorldController.scala
+++ app/uk/gov/hmrc/tradergoodsprofilesdatastore/controllers/MicroserviceHelloWorldController.scala
@@ -1,15 +1,45 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.tradergoodsprofilesdatastore.controllers
 
+import play.api.libs.json.Json
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import uk.gov.hmrc.tradergoodsprofilesdatastore.repositories.ProfileRepository
+
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton()
-class MicroserviceHelloWorldController @Inject()(cc: ControllerComponents)
-    extends BackendController(cc) {
+class MicroserviceHelloWorldController @Inject()(
+    profileRepository: ProfileRepository,
+    cc: ControllerComponents
+)(implicit ec: ExecutionContext) extends BackendController(cc) {
 
   def hello(): Action[AnyContent] = Action.async { implicit request =>
     Future.successful(Ok("Hello world"))
+
   }
+
+  def example(eori: String): Action[AnyContent] = Action.async {
+    profileRepository.get(eori).map {
+      case Some(profile) => Ok(Json.toJson(profile))
+      case None => NotFound
+    }
+  }
+
 }
