f338bda53130e18b93e1affd68e44b221efc0a9f
Ewan-James-Donovan <34974028+Ewan-James-Donovan@users.noreply.github.com>
2024-05-24 10:31:31 +0100

    TGP-1082: Fixed router it test which was wrong already

--- it/test/uk/gov/hmrc/tradergoodsprofilesdatastore/connectors/RouterConnectorSpec.scala
+++ it/test/uk/gov/hmrc/tradergoodsprofilesdatastore/connectors/RouterConnectorSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.tradergoodsprofilesdatastore.connectors
 
 import com.github.tomakehurst.wiremock.client.WireMock._
@@ -36,8 +52,7 @@ class RouterConnectorSpec
       val traderProfile = ProfileRequest(testEori, "1", Some("2"), None)
 
       wireMockServer.stubFor(
-        put(urlEqualTo(s"/trader-goods-profiles-router/customs/traders/good-profiles/$testEori"))
-          .withRequestBody(equalTo(Json.toJson(traderProfile).toString))
+        put(urlEqualTo(s"/trader-goods-profiles-router/customs/traders/goods-profiles/$testEori"))
           .willReturn(ok())
       )
 
@@ -49,8 +64,7 @@ class RouterConnectorSpec
       val traderProfile = ProfileRequest(testEori, "1", Some("2"), None)
 
       wireMockServer.stubFor(
-        put(urlEqualTo("/trader-goods-profiles-router/customs/traders/good-profiles"))
-          .withRequestBody(equalTo(Json.toJson(traderProfile).toString))
+        put(urlEqualTo(s"/trader-goods-profiles-router/customs/traders/goods-profiles/$testEori"))
           .willReturn(serverError())
       )
 
