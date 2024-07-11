3ba774109780f5d7bc48c44d49dda451029a8c6d
besscerule <63503574+besscerule@users.noreply.github.com>
2024-07-11 13:47:06 +0100

    TGP-1743: defaulted the page and size to prevent calling 10000 records everytime the page is refreshed!

--- app/uk/gov/hmrc/tradergoodsprofilesdatastore/controllers/GetRecordsController.scala
+++ app/uk/gov/hmrc/tradergoodsprofilesdatastore/controllers/GetRecordsController.scala
@@ -52,7 +52,7 @@ class GetRecordsController @Inject() (
   def getRecordsCount(
     eori: String
   ): Action[AnyContent] = identify.async { implicit request =>
-    routerConnector.getRecords(eori).map { recordsResponse =>
+    routerConnector.getRecords(eori, page = Some(1), size = Some(1)).map { recordsResponse =>
       Ok(Json.toJson(recordsResponse.pagination.totalRecords))
     }
   }
