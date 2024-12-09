57f23463b3d5ddc1431beed1321c71ef2c84de99
Reece-Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2024-12-09 10:11:53 +0000

    TGP-3050: Add a feature flag for explicitly stubbing the verified email

--- app/uk/gov/hmrc/tradergoodsprofilesdatastore/connectors/CustomsDataStoreConnector.scala
+++ app/uk/gov/hmrc/tradergoodsprofilesdatastore/connectors/CustomsDataStoreConnector.scala
@@ -32,9 +32,14 @@ class CustomsDataStoreConnector @Inject() (config: Configuration, httpClient: Ht
 ) {
 
   private val baseUrlCustomsDataStore: Service = config.get[Service]("microservice.services.customs-data-store")
+  private val stubbedCustomsDataStore: Service = config.get[Service]("microservice.services.stubbed-customs-data-store")
+  private val stubVerifiedEmail: Boolean       = config.get[Boolean]("stub-verified-email")
 
-  private def emailUrl(eori: String) =
+  private def emailUrl(eori: String) = if (stubVerifiedEmail) {
+    url"$stubbedCustomsDataStore/customs-data-store/eori/$eori/verified-email"
+  } else {
     url"$baseUrlCustomsDataStore/customs-data-store/eori/$eori/verified-email"
+  }
 
   private def eoriHistoryUrl(eori: String) =
     url"$baseUrlCustomsDataStore/customs-data-store/eori/$eori/eori-history"
