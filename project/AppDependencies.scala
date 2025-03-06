7b1e7bf1941a9b7492522b5c0328c99676adda01
Reece-Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2025-03-06 12:21:43 +0000

    TGP-3265: Upgrade dependancies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.7.0"
-  private val hmrcMongoVersion = "2.4.0"
+  private val bootstrapVersion = "9.10.0"
+  private val hmrcMongoVersion = "2.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30"         % bootstrapVersion,
