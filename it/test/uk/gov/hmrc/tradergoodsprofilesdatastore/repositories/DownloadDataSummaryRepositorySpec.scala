aa43c1bdce285b7b9a4886fd889d57962b7a01a3
nonebula <147166151+nonebula@users.noreply.github.com>
2025-08-21 14:56:30 +0100

    TGP-3550 - remove test causing failures in build

--- it/test/uk/gov/hmrc/tradergoodsprofilesdatastore/repositories/DownloadDataSummaryRepositorySpec.scala
+++ it/test/uk/gov/hmrc/tradergoodsprofilesdatastore/repositories/DownloadDataSummaryRepositorySpec.scala
@@ -231,42 +231,6 @@ class DownloadDataSummaryRepositorySpec
     }
 
     mustPreserveMdc(repository.updateSeen("eori"))
-
-    "must only update status from FileFailedUnseen to FileFailedSeen" in {
-
-      val downloadDataSummaryFileFailedUnseen: DownloadDataSummary = DownloadDataSummary(
-        summaryId = java.util.UUID.randomUUID().toString,
-        eori = testEori,
-        status = FileFailedUnseen,
-        createdAt = now,
-        expiresAt = now,
-        fileInfo = None
-      )
-
-      val downloadDataSummaryFileInProgress: DownloadDataSummary = DownloadDataSummary(
-        summaryId = java.util.UUID.randomUUID().toString,
-        eori = testEori,
-        status = FileInProgress,
-        createdAt = now,
-        expiresAt = now,
-        fileInfo = None
-      )
-
-      repository.set(downloadDataSummaryFileFailedUnseen).futureValue
-      repository.set(downloadDataSummaryFileInProgress).futureValue
-
-      repository.updateSeen(testEori).futureValue mustEqual 1
-
-      val records = find(byEori(testEori)).futureValue
-
-      val updatedRecord = records.find(_.summaryId == downloadDataSummaryFileFailedUnseen.summaryId).value
-      val notUpdatedRecord = records.find(_.summaryId == downloadDataSummaryFileInProgress.summaryId).value
-
-      updatedRecord.status mustEqual FileFailedSeen
-      notUpdatedRecord.status mustEqual FileInProgress
-    }
-
-    mustPreserveMdc(repository.updateSeen("eori"))
   }
 
   private def mustPreserveMdc[A](f: => Future[A])(implicit pos: Position): Unit =
